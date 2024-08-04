9c7d3cbb7326ce6050c6a820cecc797335d44401
rafatkab <rafat.kabir@outlook.com>
2024-08-03 22:48:41 -0400

    finished navbar and implemented home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,12 +6,14 @@ import Chat from "./pages/Chat";
 import SignUp from "./pages/SignUp";
 import Form from "./pages/Form";
 import Timeline from "./pages/Timeline";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
     <div className="flex flex-row">
       <NavBar />
       <Routes>
+        <Route path="/home" element={<Home />}></Route>
         <Route path="/chatbot" element={<Chat />}></Route>
         <Route path="/signup" element={<SignUp />}></Route>
         <Route path="/form" element={<Form />}></Route>
