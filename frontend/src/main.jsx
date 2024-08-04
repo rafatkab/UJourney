a0cc7e9839026690fc65856df5519823c45e5e36
Yash Patel <yashpatelit024@gmail.com>
2024-08-03 22:36:37 -0400

    Fix the tiniest error in Vite's env variable

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,8 +6,8 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Auth0Provider
-    domain={process.env.VITE_AUTH0_DOMAIN}
-    clientId={process.env.VITE_AUTH0_CLIENT_ID}
+    domain={import.meta.env.VITE_AUTH0_DOMAIN}
+    clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
     authorizationParams={{
       redirect_uri: window.location.origin,
     }}
